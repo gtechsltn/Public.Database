5c2e820ba2910a212ed25fb13e7387eaa5ad4738
Michael Trullas Garcia <adoser2@hotmail.es>
2023-10-22 17:01:56 +0200

    WIP

--- Data/ImageResolutions.sql
+++ Data/ImageResolutions.sql
@@ -1,7 +1,9 @@
-DECLARE @Table TABLE ([Value] NVARCHAR(25))
+DECLARE @Table TABLE ([Name] NVARCHAR(25))
 
 INSERT INTO @Table VALUES ('Original'), ('Hd'), ('FullHd')
 
 MERGE INTO ImageResolutions T USING @Table S ON S.[Name] = T.[Name]
 WHEN NOT MATCHED BY SOURCE THEN DELETE
-WHEN NOT MATCHED BY TARGET THEN INSERT ([Name]) VALUES (src.[Name]);
+WHEN NOT MATCHED BY TARGET THEN INSERT ([Name], CreatedBy, CreatedOn) VALUES (S.[Name], 1, GETDATE());
+
+GO
--- Data/Users.sql
+++ Data/Users.sql
@@ -2,5 +2,11 @@ DECLARE @Table TABLE ([Name] NVARCHAR(50), Email NVARCHAR(50))
 
 INSERT INTO @Table VALUES ('System', 'system@system.com')
 
+SET IDENTITY_INSERT Users ON
+
 MERGE INTO Users T USING @Table S ON S.Email = T.Email
-WHEN NOT MATCHED BY TARGET THEN INSERT ([Name], Email) VALUES (src.[Name], src.Email);
+WHEN NOT MATCHED BY TARGET THEN INSERT ([Id], [Name], Email, CreatedBy, CreatedOn) VALUES (1, S.[Name], S.Email, 1, GETDATE());
+
+SET IDENTITY_INSERT Users OFF
+
+GO
